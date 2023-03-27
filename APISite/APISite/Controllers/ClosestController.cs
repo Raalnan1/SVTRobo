1f6ae6b3dafdf980f9f4aef0181d2647e1dd7516
Rahsaan Pringle <Rahsaan.Pringle@Traxxis.net>
2023-03-27 13:35:38 -0400

    This commit adds a GET request

--- APISite/APISite/Controllers/ClosestController.cs
+++ APISite/APISite/Controllers/ClosestController.cs
@@ -19,6 +19,12 @@ namespace APISite.Controllers
     [ApiController]
     public class ClosestController : ControllerBase
     {
+        private object getDistance(int x, int y, Robot robot)
+        {
+            var Distance = Math.Sqrt((Math.Pow(x - robot.x, 2) + Math.Pow(y - robot.y, 2)));
+            return Distance;
+        }
+
         // POST: api/<ValuesController>
         [HttpPost]
         public async Task<Robot> AsyncPost(int x, int y)
@@ -46,10 +52,31 @@ namespace APISite.Controllers
             return sorted[0];
         }
 
-        private object getDistance(int x, int y, Robot robot)
+        // POST: api/<ValuesController>
+        [HttpGet]
+        public async Task<Robot> AsyncGet(int x, int y)
         {
-            var Distance = Math.Sqrt((Math.Pow(x - robot.x, 2) + Math.Pow(y - robot.y, 2)));
-            return Distance;
+            HttpClient client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://60c8ed887dafc90017ffbd56.mockapi.io/robots");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            //            System.Diagnostics.Debug.WriteLine(await response.Content.ReadAsStringAsync());
+            string jsonString = await response.Content.ReadAsStringAsync();
+
+            List<Robot> robots = JsonConvert.DeserializeObject<List<Robot>>(jsonString);
+
+            foreach (Robot robot in robots)
+            {
+                var distance = this.getDistance(x, y, robot);
+                robot.distance = distance;
+            }
+
+            var sorted = robots.OrderBy(x => x.distance)
+                            .ThenByDescending(x => x.batteryLevel)
+                            .ToArray();
+
+            return sorted[0];
         }
     }
 }
